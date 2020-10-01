0c802973409fff865d76f4335e65449521eb2033
itachi <enchantinggg4@gmail.com>
2020-10-02 01:59:02 +0300

    Some stuff

--- src/gateway.controller.ts
+++ src/gateway.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { Constructor, EventBus } from '@nestjs/cqrs';
 import { GatewayQueueUpdatedEvent } from './gateway/events/gateway-queue-updated.event';
+import { GatewayQueueCreatedEvent } from './gateway/events/gateway-queue-created.event';
 
 @Controller()
 export class GatewayController {
@@ -16,5 +17,10 @@ export class GatewayController {
   @EventPattern('GatewayQueueUpdatedEvent')
   async GatewayQueueUpdatedEvent(data: GatewayQueueUpdatedEvent) {
     this.event(GatewayQueueUpdatedEvent, data);
+  }
+
+  @EventPattern('GatewayQueueCreatedEvent')
+  async GatewayQueueCreatedEvent(data: GatewayQueueCreatedEvent) {
+      this.event(GatewayQueueCreatedEvent, data);
   }
 }
