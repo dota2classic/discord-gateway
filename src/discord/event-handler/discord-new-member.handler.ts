9c5f3c473dec7a8b37d243fa6f134767c9e9be41
itachi <enchantinggg4@gmail.com>
2021-01-06 10:07:16 +0300

    ok new welcome message

--- src/discord/event-handler/discord-new-member.handler.ts
+++ src/discord/event-handler/discord-new-member.handler.ts
@@ -1,17 +1,17 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { DiscordNewMemberEvent } from "../event/discord-new-member.event";
 import { Client } from "discord.js";
+import { I18nService } from "../service/i18n.service";
 
 @EventsHandler(DiscordNewMemberEvent)
 export class DiscordNewMemberHandler implements IEventHandler<DiscordNewMemberEvent> {
-  constructor(private client: Client) {}
+  constructor(private client: Client, private readonly i18n: I18nService) {}
 
   async handle(event: DiscordNewMemberEvent) {
     const u = this.client.users.resolve(event.id)
 
-    await u.send(`Привет! Я здесь, чтобы помочь тебе начать играть в классическую Dota 2!
-Для того, чтобы начать играть, нужно только начать поиск на нашем сайте https://dota2classic.ru/queue
-Игра начнется, когда наберется 10 человек и все примут игру
-Для более подробного гайда, напиши мне **!помощь**`)
+    const res = await u.send(this.i18n.welcomeMessage())
+
+    console.log(`sent`, res)
   }
 }
