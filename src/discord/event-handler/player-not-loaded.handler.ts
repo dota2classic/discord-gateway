b5c6fb246e7e1784f408dc71c1a26a210cce0548
itachi <enchantinggg4@gmail.com>
2021-01-20 22:05:55 +0300

    fix url and added tag

--- src/discord/event-handler/player-not-loaded.handler.ts
+++ src/discord/event-handler/player-not-loaded.handler.ts
@@ -2,18 +2,26 @@ import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
 import { PlayerNotLoadedEvent } from "../../gateway/events/bans/player-not-loaded.event";
 import { Client, Guild, TextChannel } from "discord.js";
 import { steamIdToNum } from "../service/util/steamids";
+import { DiscordUserRepository } from "../repository/discord-user.repository";
+import { PlayerId } from "../../gateway/shared-types/player-id";
 
 @EventsHandler(PlayerNotLoadedEvent)
 export class PlayerNotLoadedHandler
   implements IEventHandler<PlayerNotLoadedEvent> {
-  constructor(private client: Client, private readonly guild: Guild) {}
+  constructor(
+    private client: Client,
+    private readonly guild: Guild,
+    private readonly userRep: DiscordUserRepository,
+  ) {}
 
   async handle(event: PlayerNotLoadedEvent) {
     const ch = this.guild.channels.resolve('720288119227678832') as TextChannel;
+
+    const duser = this.userRep.findByPlayerId(event.playerId)
     await ch.send(
-      `Игрок https://dota2classic.ru/players/${steamIdToNum(
+      `Игрок https://dota2classic.ru/player/${steamIdToNum(
         event.playerId.value,
-      )} не загрузился в игру ${event.matchId}`,
+      )} не загрузился в игру ${event.matchId}. ${duser && `<@${duser.discordId}>`}`,
     );
   }
 }
